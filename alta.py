9ec55bf9263f779568acd14aa4c98a7a8749511d
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2019-05-23 23:02:51 -0300

    Código para as contas de alta tensão
    Funciona apenas para contas no padrão.
    Falta comentários.

---
+++ alta.py
@@ -0,0 +1,206 @@
+mypath = "C:\\Users\\LABCEE9\\Desktop\\contas\\"
+maskpath = "C:\\Users\\LABCEE9\\Desktop\\mascara.jpg"
+
+import glob2 #Módulo glob para abrir todos os arquivos .xyz do diretorio
+from PIL import Image as PILImage# Importando o módulo Pillow para abrir a imagem no script
+import pytesseract # Módulo para a utilização da tecnologia OCR
+import cv2 #Resize
+from wand.image import Image #PDF para JPG
+from os import listdir
+from os.path import isfile, join
+import os
+import csv
+
+i = 0
+j = 0
+numeroUC = []
+# pdf2jpg
+for f in listdir(mypath):
+	if f.endswith('.pdf'):
+		numeroUC.insert(i, os.path.basename(os.path.splitext(f)[0]))
+		i=i+1
+		with Image(filename=f, resolution=200) as img:
+			img.compression_quality = 99
+			if i < 10:
+				img.save(filename="contaalta"+str(0)+ str(0) +str(i)+".jpg")
+			elif i < 100:
+				img.save(filename="contaalta"+str(0)+str(i)+".jpg")
+			else:
+				img.save(filename="contaalta"+str(i)+".jpg")
+	else:
+		pass
+# Resizing
+for f in listdir(mypath):
+	if f.endswith('0.jpg'):
+		j=j+1
+		with Image(filename=f) as img:
+		    #img.resize(2480, 3508)
+		    #img.resize(1800, 2546)
+		    img.resize(4000, 5657)
+		    if j < 10:
+		    	img.save(filename= "contaaltaresized"+str(0)+ str(0) +str(j)+".jpg")
+		    elif j < 100:
+		    	img.save(filename= "contaaltaresized"+str(0)+str(j)+".jpg")		    		
+		    else:
+		    	img.save(filename= "contaaltaresized"+str(j)+".jpg")
+	else:
+		pass
+
+# -- mascara
+
+i=0
+img1 = cv2.imread(maskpath)
+try:
+	for img2 in listdir(mypath):
+		if i < 9:
+			img2 = cv2.imread('contaaltaresized'+str(0)+ str(0) +str(i+1)+'.jpg')
+			dst = img1 & img2
+			gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+			cv2.imwrite('contaalta_mascara'+str(0)+ str(0) +str(i+1)+'.jpg', gray)
+			i=i+1
+		elif i < 99:
+			img2 = cv2.imread('contaaltaresized'+str(0)+str(i+1)+'.jpg')
+			dst = img1 & img2
+			gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+			cv2.imwrite('contaalta_mascara'+str(0)+str(i+1)+'.jpg', gray)
+			i=i+1
+		else:
+			img2 = cv2.imread('contaaltaresized'+str(i+1)+'.jpg')
+			dst = img1 & img2
+			gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+			cv2.imwrite('contaalta_mascara'+str(i+1)+'.jpg', gray)
+			i=i+1
+except:
+	pass
+
+# -- jpg2txt
+
+i=1
+vetortemplate = ['Numero da UC', 'Consumo Ponta', 'Consumo Fora Ponta', 'Vencimento', 'Total em Reais\n']
+try:
+	for text in listdir(mypath):
+		vetor = []
+		if i < 10:
+			text = pytesseract.image_to_string(PILImage.open('contaalta_mascara'+str(0)+ str(0) +str(i)+'.jpg')) # Extraindo o texto da imagem
+		elif i < 100:
+				text = pytesseract.image_to_string(PILImage.open('contaalta_mascara'+str(0)+str(i)+'.jpg')) # Extraindo o texto da imagem
+		else:
+			text = pytesseract.image_to_string(PILImage.open('contaalta_mascara'+str(i)+'.jpg')) # Extraindo o texto da imagem
+		vetor = vetortemplate + text.split()
+		vetor.insert(5, numeroUC[i-1])
+		# vetor[9] = vetor[9]+' '+vetor[10]
+		vetor.pop(10)
+		#print(vetor)
+		if i < 10:
+			file = open("contaaltatxt" + str(0) + str(0) + str(i) + ".txt","a")
+		elif i < 100:
+			file = open("contaaltatxt" + str(0) + str(i) + ".txt","a")
+		else:
+			file = open("contaaltatxt" + str(i) + ".txt","a")	
+		i=i+1
+		
+		if i == 2:
+			j=0
+		else:
+		 	j=5
+
+		if vetor[8] == 'Consumo':
+			vetor[6] = vetor[6] + vetor[7]
+			vetor[10] = vetor[10] + vetor[11]
+			vetor.pop(11)
+			vetor.pop(7)
+		else:
+			vetor.pop(10)
+			vetor.pop(8)
+			vetor.pop(8)
+		#print(vetor)
+		while j < len(vetor):
+		# 	if vetor[j] == 'Total':
+		# 		file.write("")
+		# 	elif vetor[j] == 'em':
+		# 		file.write("")
+		# 	elif vetor[j] == 'Reais':
+		# 		file.write("")
+		# 	elif vetor[j] == 'Total em Reais\n':
+			if j == 4:
+				pass
+			else:
+				vetor[j] = vetor[j] + ";"
+			file.write(vetor[j])
+			j += 1
+		# 	elif (j == 10 and len(vetor) > 13):
+		# 		if (len(vetor[10]) < 7):
+		# 			fat = vetor[10] + vetor[11]
+		# 			vetor.pop(10)
+		# 			vetor.pop(10)
+		# 			while len(fat) < 7:
+		# 				fat = fat + vetor[10]
+		# 				vetor.pop(10)
+		# 		else:
+		# 			fat = vetor[10]
+		# 			vetor.pop(10)
+				
+		# 		if (len(vetor[10]) < 10):
+		# 			r = vetor[10] + vetor[11]
+		# 			vetor.pop(10)
+		# 			vetor.pop(10)
+		# 			while len(r) < 10:
+		# 				r = r + vetor[10]
+		# 				vetor.pop(10)
+		# 			r = ";" + r
+		# 		else:
+		# 			r = ";" + vetor[10]
+		# 			vetor.pop(10)
+
+		# 		if (len(vetor) > 11):
+		# 			k = vetor[10] + vetor[11]
+		# 			vetor.pop(10)
+		# 			vetor.pop(10)
+		# 			while len(vetor) > 11:
+		# 				k = k + vetor[10]
+		# 				vetor.pop(10)
+		# 			k = ";" + k
+		# 		else:
+		# 			k = ";" + vetor[10]
+		# 		file.write(fat)
+		# 		file.write(r)
+		# 		file.write(k)
+				
+		# 	else:
+		# 		if((len(vetor)==14) and (j==12)):
+		# 			file.write("")
+		# 		else:
+		# 			file.write(vetor[j]+';')
+		# 			pass
+		 	
+
+		#print(file)
+except:
+	pass
+
+# -- concat
+
+filenames = glob2.glob(mypath + '*.txt')
+
+try:
+	with open('saida.txt', 'w') as f:
+	    for file in filenames:
+	        with open(file) as infile:
+	            f.write(infile.read()+'\n')
+	        os.remove(file)
+except:
+	pass
+
+# txt 2 csv
+
+with open('saida.txt', 'r') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split(",") for line in stripped if line)
+    with open('saida2.csv', 'w', newline='') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerows(lines)
+
+# Remove jpg
+filenames = glob2.glob(mypath + '*.jpg')
+for file in filenames:
+ 	os.remove(file)
